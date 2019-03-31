450a248aec08396682bad61ed3aba34c37bd7331
perison15 <48751178+perison15@users.noreply.github.com>
2019-03-31 09:32:13 +0200

    greyhound dogs race algorithm

--- practise/objects/index.js
+++ practise/objects/index.js
@@ -1,6 +1,17 @@
-let anObject = {left :1,right :2};
-console.log(anObject.left);
-delete anObject.left;
-console.log(anObject.left);
-console.log("left" in anObject);
-console.log("right" in anObject);
+function dogs() {
+    let a = Math.floor(Math.random() * 7);
+    let b = Math.floor(Math.random() * 7);
+    let c = Math.floor(Math.random() * 7);
+
+        
+            if (a !== b && b!==c && a !== c && a !==0 && b !==0 && c!== 0) {
+            return a +"," + b +","+c;
+         }
+         else {
+             return dogs();
+         }
+
+}
+console.log(dogs());
+
+
