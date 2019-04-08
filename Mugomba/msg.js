7b1afe0f1b285266cc2bbd0029a0a690787e80a1
perison15 <48751178+perison15@users.noreply.github.com>
2019-04-08 18:28:38 +0200

    changing colors

---
+++ Mugomba/msg.js
@@ -0,0 +1,17 @@
+
+// window.addEventListener("click", event => {
+//      let dot = document.createElement("div"); 
+//      dot.className = "dot"; 
+//      document.body.style.background = "violet";
+
+//     //  dot.style.left = (event.pageX - 4) + "px"; 
+//     //  dot.style.top = (event.pageY - 4) + "px"; 
+//      document.body.appendChild(dot);
+// });
+let lights = document.getElementById("mes");
+console.log(lights.style.color);
+function me() {
+    // lights.style.color = "blue";
+    // lights.style.background="white";
+    lights.style.color="blue";
+}
